df0f265ed7285ecc30397e14a4ef3e7a5fd47fda
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2021-03-10 00:57:24 -0300

    unary conditional

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 
-const buttonA = <button>Primeiro Botão</button>
-const buttonB =  <button>Segundo Botão</button>
+const buttonA = <button>Histórico de Clientes</button>
+//const buttonB =  <button>Segundo Botão</button>
+
+const hasCustomer = true;
 
 const App = () => {
   return (
     <div>
       <p>Código Fluente</p>
       <p>Seja bem-vindo</p>
-      {buttonA}
-      {buttonB}
+      { hasCustomer &&
+        (
+          <div>
+            Clique no botão abaixo para visualizar o histórico de clientes
+            <br />
+            {buttonA}
+          </div>
+          )
+      }
     </div>
   );
 };
