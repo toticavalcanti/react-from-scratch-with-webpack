3fe245439f516d5b19f079df66b22cbbd6801f41
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2021-03-10 01:15:20 -0300

    ternary conditional

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,53 @@
 import React from 'react';
 
 const buttonA = <button>Histórico de Clientes</button>
-//const buttonB =  <button>Segundo Botão</button>
+const buttonB =  <button>Cadastrar Cliente</button>
 
 const hasCustomer = true;
 
 const App = () => {
+
+  const renderShowHistory = () => {
+    return (
+      <div>
+        Clique no botão abaixo para visualizar o histórico de clientes
+        <br />
+        {buttonA}
+      </div>
+      ) 
+  }
+
+  const renderAddCustomer = () => {
+    return (
+      <div>
+        Clique abaixo para cadastrar o cliente
+        <br />
+        {buttonB}
+      </div>
+    )
+  }
+
+  const customer = 'Toti Cavalcanti'
+
+  const showCustomer = () => {
+    if(!hasCustomer) return null;
+    return(
+      <div>
+        <h1>
+          Nome do cliente: {customer}
+        </h1>
+      </div>
+    )
+  }
+
   return (
     <div>
       <p>Código Fluente</p>
       <p>Seja bem-vindo</p>
-      { hasCustomer &&
-        (
-          <div>
-            Clique no botão abaixo para visualizar o histórico de clientes
-            <br />
-            {buttonA}
-          </div>
-          )
-      }
+      { hasCustomer ? renderShowHistory() : renderAddCustomer()}
+      <div>
+        {showCustomer()}
+      </div>
     </div>
   );
 };
