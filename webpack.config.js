02ee2894700848cd19631e746e7600599e74b3f4
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2021-03-10 00:38:26 -0300

    insert source-map in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 module.exports = {
+  devtool: 'source-map',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
