36551d3d4f3dd134b803423d58663f8c8ed6ac2d
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2021-03-10 12:09:25 -0300

    move showEvent() and Button into App, the arraw function has this scope encapsulate

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,29 @@
 import React from 'react';
 
-const listCustomers = [
-  {
-    id: 1,
-    name: 'Toti Cavalcanti',
-    skills: ['React', 'Node', 'Python', 'Django']
-  },
-  {
-    id: 2,
-    name: 'Walter de Lucca Junior',
-    skills: ['C', 'C++', 'React', 'Angular']
-  },
-  {
-    id: 3,
-    name: 'Carlos Augusto de Souza',
-    skills: ['Webpack', 'Java', 'React Native', 'Angular']
-  },
-  {
-    id: 14,
-    name: 'Elisabeth Gonçalves',
-    skills: ['R', 'elixir', 'React', 'Angular']
-  },
-
-]
-
 const App = () => {
+  
+  const name = "Código Fluente";
 
-  const renderCustomers = (customer) => {
-
-    return (
-      <div key={`customer-${customer.id}`}>
-        <li>{ customer.name }</li>
-        {customer.skills.map(renderSkills)}
+  const handleChange = (e) => {
+    const { value } = e.target
+    console.log(value)
+  }
+  
+  const showEvent = (e) => {
+    alert('Evento clicado!')
+    console.log(e)
+    name
+  }
+  
+  const Button = <button onClick={showEvent}>Mostrar Evento</button>
+  
+  return(
+      <div>
+        <p>Código Fluente</p>
+        <p>Bem-vindo</p>
+        <input onChange={handleChange} />
+        {Button}
       </div>
     )
   }
-
-  const renderSkills = (skill, index) => {
-    return (
-      <div style={{paddingLeft: '30px'}}>
-        <li key={`skill-${index}`}>{ skill }</li>
-      </div>
-
-    )
-  }
-
-  return (
-    <div>
-      <p>Código Fluente</p>
-      <p>Seja bem-vindo</p>
-      <div>{listCustomers.map(renderCustomers)}</div>
-    </div>
-  );
-} ;
-
 export default App;
